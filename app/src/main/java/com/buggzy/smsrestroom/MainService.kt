0985c8e82e6cf3dd3773b35b00b2422a020d548a
isavnin <vladkalyuzhnyu@gmail.com>
2019-05-21 07:18:49 +0300

    changes

--- app/src/main/java/com/buggzy/smsrestroom/MainService.kt
+++ app/src/main/java/com/buggzy/smsrestroom/MainService.kt
@@ -23,9 +23,9 @@ class MainService : BaseService() {
 
     private val smsDisposable = CompositeDisposable()
 
-    private var delay: Long? = null
+    private var smsDelay: Long? = null
 
-    private var time = -1L
+    private var lastTime = -1L
 
     override fun onCreate() {
         super.onCreate()
@@ -44,42 +44,40 @@ class MainService : BaseService() {
                 (application as MainApp).api.pingStatus("${Preferences.baseUrl}/path/to/status", androidId)
                     .subscribeOn(Schedulers.io())
                     .subscribe({}, {})
-                if () {
-
+                smsDelay = if (smsDelay == null) 0L else (smsDelay ?: 0L) + PING_INTERVAL
+                if (smsDelay ?: 0L < SMS_TIMEOUT) {
+                    return@subscribe
                 }
-                delay += PING_INTERVAL
-                if (delay >= 60_000L) {
-                    delay = 0
-                    smsDisposable.clear()
-                    time = System.currentTimeMillis()
-                    smsDisposable.add(RxCursorLoader.create(contentResolver, smsQuery)
-                        .subscribe({ cursor ->
-                            cursor.use {
-                                Timber.d("content://sms ${it.count}")
-                                if (it.getInt(it.getColumnIndexOrThrow(Telephony.Sms.TYPE))
-                                    != Telephony.Sms.MESSAGE_TYPE_INBOX) {
-                                    Timber.i("Last sms is outcoming")
-                                    return@use
-                                }
-                                val smsTime = it.getLong(it.getColumnIndexOrThrow(Telephony.Sms.DATE))
-                                if (smsTime <= time) {
-                                    Timber.i("Last sms time is earlier or the same: %d <= %d",
-                                        smsTime, time)
-                                    return@use
-                                }
-                                time = smsTime
-                                val from = it.getString(it.getColumnIndexOrThrow(Telephony.Sms.ADDRESS))
-                                val body = it.getString(it.getColumnIndexOrThrow(Telephony.Sms.BODY))
-                                (application as MainApp).api.sendSms("${Preferences.baseUrl}/path/to/sms",
-                                    androidId, smsTime, from, body)
-                                    .subscribeOn(Schedulers.io())
-                                    .subscribe({}, {})
+                smsDelay = 0L
+                smsDisposable.clear()
+                lastTime = System.currentTimeMillis()
+                smsDisposable.add(RxCursorLoader.create(contentResolver, smsQuery)
+                    .subscribe({ cursor ->
+                        cursor.use {
+                            Timber.d("content://sms ${it.count}")
+                            if (it.getInt(it.getColumnIndexOrThrow(Telephony.Sms.TYPE))
+                                != Telephony.Sms.MESSAGE_TYPE_INBOX) {
+                                Timber.i("Last sms is outcoming")
+                                return@use
                             }
-                        }, {
-                            Timber.e(it)
-                            showToast(it.message.toString())
-                        }))
-                }
+                            val smsTime = it.getLong(it.getColumnIndexOrThrow(Telephony.Sms.DATE))
+                            if (smsTime <= lastTime) {
+                                Timber.i("Last sms time is earlier or the same: %d <= %d",
+                                    smsTime, lastTime)
+                                return@use
+                            }
+                            lastTime = smsTime
+                            val from = it.getString(it.getColumnIndexOrThrow(Telephony.Sms.ADDRESS))
+                            val body = it.getString(it.getColumnIndexOrThrow(Telephony.Sms.BODY))
+                            (application as MainApp).api.sendSms("${Preferences.baseUrl}/path/to/sms",
+                                androidId, smsTime, from, body)
+                                .subscribeOn(Schedulers.io())
+                                .subscribe({}, {})
+                        }
+                    }, {
+                        Timber.e(it)
+                        showToast(it.message.toString())
+                    }))
             })
         return START_NOT_STICKY
     }
@@ -107,5 +105,10 @@ class MainService : BaseService() {
     override fun onDestroy() {
         smsDisposable.dispose()
         super.onDestroy()
+    }
+
+    companion object {
+
+        private const val SMS_TIMEOUT = 60_000L
     }
 }
